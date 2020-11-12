af52d6c8ced505d7e7bbd9a99099e9e6c4a53452
sebbby <seb.beitel@gmail.com>
2020-11-12 13:29:03 -0500

    add flow to switchboard view

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import State, Question, Result
+from datetime import datetime, timezone, timedelta
+
+# TO DEFINE LENGTH OF TIME FOR QUESTION AND INTERMISSION PERIOD
+question_time = 30000
+intermission_time = 10000
 
 def switchboard(request):
-  state = State.objects.all().first()
+  state = State.objects.first()
   if state.current_state == 'question':
-    return redirect('question')
+    time_elapsed = (datetime.now(timezone.utc) - state.time_stamp) / timedelta(microseconds=1) / 1000
+    if time_elapsed > question_time:
+      state.current_state = 'intermission'
+      state.time_stamp = datetime.now(timezone.utc)
+      state.save()
+      return redirect('intermission')
+    if time_elapsed < question_time:
+      return redirect('question')
   if state.current_state == 'intermission':
-    return redirect('intermission')
+    time_elapsed = (datetime.now(timezone.utc) - state.time_stamp)  / timedelta(microseconds=1) / 1000
+    if time_elapsed > intermission_time:
+      state.current_state = 'question'
+      state.time_stamp = datetime.now(timezone.utc)
+      state.save()
+      return redirect('question')
+    if time_elapsed < question_time:
+      return redirect('intermission')
   if state.current_state == 'waiting':
     return redirect('waiting')
 
