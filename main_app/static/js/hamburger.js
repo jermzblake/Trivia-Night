fc7363da5082ef359d8fe2ee7341626831dde8e6
nickmackenzie <nick-m@live.ca>
2020-11-15 02:21:44 -0500

    add local storage for menu state

--- main_app/static/js/hamburger.js
+++ main_app/static/js/hamburger.js
@@ -1,7 +1,21 @@
 let hamburger = document.getElementById("ham");
 let sideMenu = document.getElementById("side-menu");
+let menuState = localStorage.getItem("state");
+
 hamburger.addEventListener("click", sideMenuShow);
 
-function sideMenuShow() {
-  sideMenu.classList.toggle("not-active");
+function menuSet() {
+  sideMenu.className = menuState;
 }
+
+function sideMenuShow() {
+  if (sideMenu.className == "not-active") {
+    sideMenu.classList.toggle("not-active");
+    localStorage.setItem("state", "");
+  } else {
+    sideMenu.classList.toggle("not-active");
+    localStorage.setItem("state", "not-active");
+  }
+}
+
+menuSet();
