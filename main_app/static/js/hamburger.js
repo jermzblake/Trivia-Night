51d42745969277cde0432a379daa217048102b94
nickmackenzie <nick-m@live.ca>
2020-11-15 02:39:37 -0500

    hamburger menu stays open during refreshes

--- main_app/static/js/hamburger.js
+++ main_app/static/js/hamburger.js
@@ -4,18 +4,21 @@ let menuState = localStorage.getItem("state");
 
 hamburger.addEventListener("click", sideMenuShow);
 
+// This grabs the hamburger menu's state from localstorage
 function menuSet() {
   sideMenu.className = menuState;
 }
 
+// Toggling the two states of the menu in local storage and front end side
 function sideMenuShow() {
-  if (sideMenu.className == "not-active") {
-    sideMenu.classList.toggle("not-active");
-    localStorage.setItem("state", "");
+  if (sideMenu.className == "closed") {
+    sideMenu.classList.toggle("closed");
+    localStorage.setItem("state", "open");
   } else {
-    sideMenu.classList.toggle("not-active");
-    localStorage.setItem("state", "not-active");
+    sideMenu.classList.toggle("closed");
+    localStorage.setItem("state", "closed");
   }
 }
 
+// Setting the menu every reload
 menuSet();
