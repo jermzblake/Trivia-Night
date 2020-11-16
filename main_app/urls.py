87677bee0d063dfab0779c7a6e23f12f87a40398
Jermaine <jermzblake@gmail.com>
2020-11-16 17:34:39 -0500

    add profile paths and add profile create link to detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
   path('waiting/<int:result_id>', views.waiting, name='waiting'),
   path('accounts/signup/', views.signup, name='signup'),
   path('detail/<int:user_id>', views.profile_detail, name='detail'),
+  path('profile/create', views.ProfileCreate.as_view(), name='profiles_create'),
+  path('profile/<int:pk>/update/', views.ProfileUpdate.as_view(), name='profiles_update'),
+  path('profile/<int:pk>/delete/', views.ProfileDelete.as_view(), name='profiles_delete'),
   path('play', views.play, name='play'),
   path('info', views.info, name='info'),
-  path('users/<int:profile_id>/add_photo', views.add_photo, name='add_photo'),
+  path('profile/<int:profile_id>/add_photo', views.add_photo, name='add_photo'),
 ]
