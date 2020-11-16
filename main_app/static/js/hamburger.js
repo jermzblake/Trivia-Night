dc632fe8d78b0e58cf59b7bd4f3eb6e283e2df24
nickmackenzie <nick-m@live.ca>
2020-11-16 11:47:53 -0500

    removed hamburger js and put them in individual files
    this was causing var errors when it was in base.html

--- main_app/static/js/hamburger.js
+++ main_app/static/js/hamburger.js
@@ -2,9 +2,18 @@ let hamburger = document.getElementById("ham");
 let sideMenu = document.getElementById("side-menu");
 let menuState = localStorage.getItem("state");
 
+let selector = document.getElementById("selector");
+
+let hour = document.querySelector(".hour");
+let day = document.querySelector(".day");
+let week = document.querySelector(".week");
+let month = document.querySelector(".month");
+let alltime = document.querySelector(".alltime");
+
+selector.addEventListener("click", leaderboardChange);
+
 hamburger.addEventListener("click", sideMenuShow);
 
-// This grabs the hamburger menu's state from localstorage
 function menuSet() {
   sideMenu.className = menuState;
 }
@@ -20,5 +29,36 @@ function sideMenuShow() {
   }
 }
 
+function leaderboardChange(e) {
+  userChoice = e.target.value;
+  if (userChoice === "hour") {
+    hour.style.display = "block";
+    day.style.display = "none";
+    week.style.display = "none";
+    month.style.display = "none";
+    alltime.style.display = "none";
+  }
+  if (userChoice === "day") {
+    hour.style.display = "none";
+    day.style.display = "block";
+    week.style.display = "none";
+    month.style.display = "none";
+    alltime.style.display = "none";
+  }
+  if (userChoice === "month") {
+    hour.style.display = "none";
+    day.style.display = "none";
+    week.style.display = "block";
+    month.style.display = "none";
+    alltime.style.display = "none";
+  }
+  if (userChoice === "alltime") {
+    hour.style.display = "none";
+    day.style.display = "none";
+    week.style.display = "none";
+    month.style.display = "none";
+    alltime.style.display = "block";
+  }
+}
 // Setting the menu every reload
 menuSet();
