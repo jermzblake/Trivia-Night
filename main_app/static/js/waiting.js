a84301e7593d2cb5dd3df12c7367b997d7efc0b0
sebbby <seb.beitel@gmail.com>
2020-11-17 14:04:43 -0500

    added ajax to waiting page to refresh the scoreboard

--- main_app/static/js/waiting.js
+++ main_app/static/js/waiting.js
@@ -3,6 +3,27 @@ setTimeout (function() {
   window.location.href = '/switchboard'
 }, time_left)
 
-setInterval(function() {
-  window.location.href = `/waiting/${result_id}`
-},2500)
+refreshScoreboard()
+setInterval(refreshScoreboard,2500)
+
+listEl = document.getElementById('scoreboard')
+
+async function refreshScoreboard() {
+  await fetch('/refresh_scoreboard', {
+    headers:{
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+    },
+  })
+  .then(response => {
+      return response.json()
+  })
+  .then(data => {
+      listEl.innerHTML = ''
+      for (const [key, value] of Object.entries(data)) {
+        let scoreboardItem = document.createElement('h5')
+        scoreboardItem.textContent =  `${key} - ${value}`
+        listEl.appendChild(scoreboardItem)
+      }
+  })
+}
