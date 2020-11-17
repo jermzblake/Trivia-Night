21a1e448c20c5531bdbb5bcae0d86d0cb5c60f42
sebbby <seb.beitel@gmail.com>
2020-11-17 17:25:27 -0500

    add styled scoreboard table

--- main_app/static/js/waiting.js
+++ main_app/static/js/waiting.js
@@ -1,4 +1,4 @@
-// Set a timer to direct user to the switchboard at next scheduled state change
+Set a timer to direct user to the switchboard at next scheduled state change
 setTimeout (function() {
   window.location.href = '/switchboard'
 }, time_left)
@@ -21,9 +21,15 @@ async function refreshScoreboard() {
   .then(data => {
       listEl.innerHTML = ''
       for (const [key, value] of Object.entries(data)) {
-        let scoreboardItem = document.createElement('h5')
-        scoreboardItem.textContent =  `${key} - ${value}`
+        let htmlBlock = 
+          '<div class="score-tile">' +
+            `<div class="score-tile-name">${key}</div>` +
+            `<div class="score-tile-score">${value}</div>` +
+          '</div>'
+        let scoreboardItem = document.createElement('div')
+        scoreboardItem.innerHTML =  htmlBlock
         listEl.appendChild(scoreboardItem)
       }
   })
 }
+
