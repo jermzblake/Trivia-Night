07385c5f5fc590be5f03847e60a3100aa99325ae
nickmackenzie <nick-m@live.ca>
2020-11-18 12:45:33 -0500

    added buttons and spaced it to the center

--- main_app/static/js/hamburger.js
+++ main_app/static/js/hamburger.js
@@ -4,17 +4,25 @@ let sideMenu = document.getElementById("side-menu");
 let menuState = localStorage.getItem("state");
 let leaderboardState = localStorage.getItem("leaderboard");
 
-let selector = document.getElementById("selector");
+let hour = document.querySelector("#hour");
+let day = document.querySelector("#day");
+let week = document.querySelector("#week");
+let month = document.querySelector("#month");
+let alltime = document.querySelector("#alltime");
 
-let hour = document.querySelector(".hour");
-let day = document.querySelector(".day");
-let week = document.querySelector(".week");
-let month = document.querySelector(".month");
-let alltime = document.querySelector(".alltime");
-
-selector.addEventListener("click", leaderboardChange);
+let hourBoard = document.querySelector(".hour_board");
+let dayBoard = document.querySelector(".day_board");
+let weekBoard = document.querySelector(".week_board");
+let monthBoard = document.querySelector(".month_board");
+let alltimeBoard = document.querySelector(".alltime_board");
 
 hamburger.addEventListener("click", sideMenuShow);
+
+hour.addEventListener("click", leaderboardChange);
+day.addEventListener("click", leaderboardChange);
+week.addEventListener("click", leaderboardChange);
+month.addEventListener("click", leaderboardChange);
+alltime.addEventListener("click", leaderboardChange);
 
 function menuSet() {
   sideMenu.className = menuState;
@@ -32,49 +40,49 @@ function sideMenuShow() {
 }
 
 function leaderboardChange(e) {
-  userChoice = e.target.value;
+  let userChoice = e.target.value;
   if (userChoice === "hour") {
-    hour.style.display = "block";
-    day.style.display = "none";
-    week.style.display = "none";
-    month.style.display = "none";
-    alltime.style.display = "none";
+    hourBoard.style.display = "block";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "none";
+    alltimeBoard.style.display = "none";
     localStorage.setItem("leaderboard", "hour");
     localStorage.setItem("index", "0");
   }
   if (userChoice === "day") {
-    hour.style.display = "none";
-    day.style.display = "block";
-    week.style.display = "none";
-    month.style.display = "none";
-    alltime.style.display = "none";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "block";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "none";
+    alltimeBoard.style.display = "none";
     localStorage.setItem("leaderboard", "day");
     localStorage.setItem("index", "1");
   }
   if (userChoice === "week") {
-    hour.style.display = "none";
-    day.style.display = "none";
-    week.style.display = "block";
-    month.style.display = "none";
-    alltime.style.display = "none";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "block";
+    monthBoard.style.display = "none";
+    alltimeBoard.style.display = "none";
     localStorage.setItem("leaderboard", "week");
     localStorage.setItem("index", "2");
   }
   if (userChoice === "month") {
-    hour.style.display = "none";
-    day.style.display = "none";
-    week.style.display = "none";
-    month.style.display = "block";
-    alltime.style.display = "none";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "block";
+    alltimeBoard.style.display = "none";
     localStorage.setItem("leaderboard", "month");
     localStorage.setItem("index", "3");
   }
   if (userChoice === "alltime") {
-    hour.style.display = "none";
-    day.style.display = "none";
-    week.style.display = "none";
-    month.style.display = "none";
-    alltime.style.display = "block";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "none";
+    alltimeBoard.style.display = "block";
     localStorage.setItem("leaderboard", "alltime");
     localStorage.setItem("index", "4");
   }
@@ -84,60 +92,55 @@ currentBoard = localStorage.getItem("leaderboard");
 localStorage.setItem("leaderboard", currentBoard);
 
 function leaderboardStateChange() {
-  console.log(currentBoard);
   currentSelect = localStorage.getItem("index");
   if (currentBoard == "hour") {
-    hour.style.display = "block";
-    day.style.display = "none";
-    week.style.display = "none";
-    month.style.display = "none";
-    alltime.style.display = "none";
+    hourBoard.style.display = "block";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "none";
+    alltimeBoard.style.display = "none";
     localStorage.setItem("leaderboard", "hour");
-    selector.selectedIndex = currentSelect;
   }
   if (currentBoard == "day") {
-    hour.style.display = "none";
-    day.style.display = "block";
-    week.style.display = "none";
-    month.style.display = "none";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "block";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "none";
     alltime.style.display = "none";
     localStorage.setItem("leaderboard", "day");
-    selector.selectedIndex = currentSelect;
   }
   if (currentBoard == "week") {
-    hour.style.display = "none";
-    day.style.display = "none";
-    week.style.display = "block";
-    month.style.display = "none";
-    alltime.style.display = "none";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "block";
+    monthBoard.style.display = "none";
+    alltimeBoard.style.display = "none";
     localStorage.setItem("leaderboard", "week");
-    selector.selectedIndex = currentSelect;
   }
   if (currentBoard == "month") {
-    hour.style.display = "none";
-    day.style.display = "none";
-    week.style.display = "none";
-    month.style.display = "block";
-    alltime.style.display = "none";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "block";
+    alltimeBoard.style.display = "none";
     localStorage.setItem("leaderboard", "month");
-    selector.selectedIndex = currentSelect;
   }
   if (currentBoard == "alltime") {
-    hour.style.display = "none";
-    day.style.display = "none";
-    week.style.display = "none";
-    month.style.display = "none";
-    alltime.style.display = "block";
+    hourBoard.style.display = "none";
+    dayBoard.style.display = "none";
+    weekBoard.style.display = "none";
+    monthBoard.style.display = "none";
+    alltimeBoard.style.display = "block";
     localStorage.setItem("leaderboard", "alltime");
-    selector.selectedIndex = currentSelect;
   }
 }
-window.addEventListener("click", function (e) {
-  if (e.target != hamburger && e.target != selector && e.target != sideMenu) {
-    sideMenu.classList.add("closed");
-    localStorage.setItem("state", "closed");
-  }
-});
+// window.addEventListener("click", function (e) {
+//   if (e.target != hamburger && e.target != sideMenu) {
+//     sideMenu.classList.add("closed");
+//     localStorage.setItem("state", "closed");
+//   }
+// });
+
 // Setting the menu every reload
 menuSet();
 leaderboardStateChange();
